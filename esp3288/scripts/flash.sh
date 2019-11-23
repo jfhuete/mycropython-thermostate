da1d1409c72e1a0ae54cc668ec36f852c60bec2c
Juan Francisco Huete Verdejo <jf.huete@doofinder.com>
2019-11-23 20:10:34 +0100

    Added all scripts files to exclude list

--- esp3288/scripts/flash.sh
+++ esp3288/scripts/flash.sh
@@ -4,7 +4,7 @@ set -e
 
 source scripts/config
 
-IGNORED=[".micropythonrc","scripts/"]
+IGNORED=[".micropythonrc","scripts/flash.sh","scripts/config","scripts/run.sh","../.gitignore"]
 
 CHANGED_FILES=`git diff HEAD --name-status --no-renames | awk '$1 != "D" {print $2}' | sed -e 's/esp3288\///g'`
 DELETED_FILES=`git diff HEAD --name-status --no-renames | awk '$1 == "D" {print $2}' | sed -e 's/esp3288\///g'`
