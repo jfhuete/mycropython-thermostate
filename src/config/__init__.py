c2963c017d9c9df068bf153086a83e72a6f8aa30
Juan Francisco Huete Verdejo <jf.huete@gmail.com>
2020-10-18 20:05:35 +0200

    Temperature sensor working

---
+++ src/config/__init__.py
@@ -0,0 +1,1 @@
+from config.config import * # NOQA F401 F403
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from thermostate import Thermostate
 from net import Net
 
+
 if __name__ == "__main__":
     n = Net()
     n.connect()
