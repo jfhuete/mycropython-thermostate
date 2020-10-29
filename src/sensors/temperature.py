165666118058cade5ca13e2c273be63ba440ca72
Juan Francisco Huete Verdejo <jf.huete@gmail.com>
2020-10-29 19:59:22 +0100

    Added medium temperature

--- src/sensors/temperature.py
+++ src/sensors/temperature.py
@@ -10,17 +10,56 @@ class Temperature:
         self.sensor = dht.DHT22(PIN_TEMP_SENSOR)
         self.logger = Logger(self.state, "TEMP")
 
+        self.__temp_meassures = []
+        self.__humidity_meassures = []
+
     def measure(self):
         try:
             self.sensor.measure()
 
             temp = self.sensor.temperature()
             humidity = self.sensor.humidity()
-            self.logger.info("Temp: {} ºC, Humid: {}%".format(temp, humidity))
+
+            self.__add_temperature(temp, humidity)
+
+            temp_mean = self.__get_temperature()
+            humidity_mean = self.__get_humidity()
+
+            self.logger.info("Temp: {} ºC, Humid: {}%".format(
+                temp_mean, humidity_mean))
         except OSError as e:
-            temp = None
-            humidity = None
+            temp_mean = None
+            humidity_mean = None
             self.logger.error("Can't to read temperature {}".format(e))
 
-        self.state.temperature_measured = temp
-        self.state.humidity_measured = humidity
+        self.state.temperature_measured = temp_mean
+        self.state.humidity_measured = humidity_mean
+
+    def __add_temperature(self, temp, humidity):
+
+        self.__temp_meassures.append(temp)
+        self.__humidity_meassures.append(humidity)
+
+        if len(self.__temp_meassures) > 50:
+            self.__temp_meassures.pop(0)
+
+        if len(self.__humidity_meassures) > 50:
+            self.__humidity_meassures.pop(0)
+
+    def __get_temperature(self):
+
+        acum = 0
+
+        for temp in self.__temp_meassures:
+            acum += temp
+
+        return round(acum / len(self.__temp_meassures), 2)
+
+    def __get_humidity(self):
+
+        acum = 0
+
+        for humidity in self.__humidity_meassures:
+            acum += humidity
+
+        return round(acum / len(self.__humidity_meassures), 2)
